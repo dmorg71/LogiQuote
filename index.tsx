2625bb01bbcf6e90ca6918e9fcfb2e3f24820518
dmorg71 <dmorg71@gmail.com>
2025-07-29 21:59:50 -0400

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
+      <h1>🚀 Welcome to LogiQuote</h1>
+      <p>Your quoting engine is now live and ready to build!</p>
+    </div>
+  );
+}
