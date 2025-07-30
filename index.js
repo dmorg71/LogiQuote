75551b102eae0116483809a113eab92b86524f4d
dmorg71 <dmorg71@gmail.com>
2025-07-29 22:35:46 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
 export default function Home() {
   return (
-    <div style={{ padding: 40 }}>
-      <h1>Welcome to LogiQuote!</h1>
-      <p>This is your final expense quoting system.</p>
+    <div>
+      <h1>Welcome to LogiQuote</h1>
+      <p>This is a working JavaScript-only deployment. Let's build.</p>
     </div>
   );
 }
