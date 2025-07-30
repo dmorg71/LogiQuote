e0b7c4d45c366bce83e69df1a2dac6ca84b1f496
dmorg71 <dmorg71@gmail.com>
2025-07-29 23:59:41 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
-
 import React from 'react';
 
 function App() {
   return (
-    <div>
-      <h1>LogiQuote App</h1>
-      <p>This build will succeed.</p>
+    <div className="App">
+      <h1>LogiQuote: Fast Winning Quotes</h1>
     </div>
   );
 }
