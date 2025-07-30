191d6f020e3902997e13d0a157d2867e5cc2eef7
dmorg71 <dmorg71@gmail.com>
2025-07-29 23:46:38 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
+
 import React from 'react';
 
 function App() {
   return (
-    <div style={{ padding: 40, fontSize: 24 }}>
-      <h1>🚀 LogiQuote is Live</h1>
-      <p>Enter a face amount, age, or condition to get fast winning quotes.</p>
+    <div>
+      <h1>LogiQuote App</h1>
+      <p>This build will succeed.</p>
     </div>
   );
 }
